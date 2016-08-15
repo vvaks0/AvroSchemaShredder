da436a14f42e7252ad20220a30ce3f2604d98ab0
vvaks <vvaks@hortonworks.com>
2016-08-14 23:44:47 -0400

    install and samples

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+
+mvn clean package
+
+mv target/AvroSchemaShredder-jar-with-dependencies.jar ./
